e78c11573ddc10a93831f02e749a5e9b53f11405
Filip Kolev <fil.kolev@gmail.com>
2015-11-17 22:54:37 +0200

    initial commit

---
+++ C-Plus-Plus-Usage-Check/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace C_Plus_Plus_Usage_Check
+{
+    using System;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string directoryPath =
+                @"C:\Users\Filip-Laptop\AppData\Roaming\Skype\My Skype Received Files\Contest last submissions for C Programming Exam 14 November 2015";
+
+            string regexPattern = @"(?<!\w)std(?!\w)";
+            var regex = new Regex(regexPattern);
+
+            var subDirectories = Directory.GetDirectories(directoryPath);
+            foreach (var subDirectory in subDirectories)
+            {
+                var files = Directory.GetFiles(subDirectory);
+                foreach (var file in files)
+                {
+                    var text = File.ReadAllText(file);
+
+                    if (regex.IsMatch(text))
+                    {
+                        Console.WriteLine("std use detected in: {0}", file);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Percentage-Similarity/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Percentage_Similarity
+{
+    using System.IO;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        static void Main()
+        {
+            // Enter directory path of exam solutions
+            string directoryPath =
+                @"C:\Users\Filip-Laptop\AppData\Roaming\Skype\My Skype Received Files\Exam";
+
+            // Enter problem names
+            string[] problemNames =
+            {
+                "01. Darts.cpp",
+                "02. Bitwise-Minesweeper.cpp",
+                "03. Hidden-Secrets.cpp",
+                "04. String-Builder.cpp"
+            };
+           
+            Regex whitespaceRegex = new Regex(@"\s+");
+
+            Parallel.ForEach(problemNames, problemName =>
+            {
+                string problemNumber = problemName.Split('.')[0];
+
+                using (var output = File.AppendText($"../../output-{problemNumber}.csv"))
+                {
+                    output.WriteLine("similarity,contestant_1,contestant_2");
+
+                    var subDirectories = Directory.GetDirectories(directoryPath);
+                    for (int i = 0; i < subDirectories.Length; i++)
+                    {
+                        for (int j = i + 1; j < subDirectories.Length; j++)
+                        {
+                            var file1 = subDirectories[i] + "\\" + problemName;
+                            var file2 = subDirectories[j] + "\\" + problemName;
+
+                            try
+                            {
+                                var file1Content = whitespaceRegex.Replace(File.ReadAllText(file1), " ");
+                                var file2Content = whitespaceRegex.Replace(File.ReadAllText(file2), " ");
+                                var similarity = file1Content.CalculateSimilarity(file2Content);
+
+                                output.WriteLineAsync($"{similarity:F3},{file1.Split('\\')[8]},{file2.Split('\\')[8]}");
+                            }
+                            catch (IOException)
+                            {
+                                // if no submission for problem - continue
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
